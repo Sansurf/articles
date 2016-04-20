6c7c6accc3c8187103f4fa81d24adce60d8578dd
Sansurf <sansurf@inbox.ru>
2016-04-20 08:23:28 +0600

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,36 @@
-<?phpfunction __autoload($classname){	include_once("c/$classname.php");}$action = 'action_';$action .= (isset($_GET['act'])) ? $_GET['act'] : 'index';$left = 'left_';$left .= (isset($_GET['l'])) ? $_GET['l'] : 'view';if (isset($_GET['c'])){	switch ($_GET['c'])	{		case 'C_Page':			$controller = new C_Page();			break;		case 'C_Edit':			$controller = new C_Edit();			break;		default:			$controller = new C_Page();	}}else{	$controller = new C_Page();}$controller->Request($action, $left);?>
+<?php
+
+// check VCS
+
+function __autoload($classname){
+	include_once("c/$classname.php");
+}
+
+$action = 'action_';
+$action .= (isset($_GET['act'])) ? $_GET['act'] : 'index';
+
+$left = 'left_';
+$left .= (isset($_GET['l'])) ? $_GET['l'] : 'view';
+
+if (isset($_GET['c']))
+{
+	switch ($_GET['c'])
+	{
+		case 'C_Page':
+			$controller = new C_Page();
+			break;
+		case 'C_Edit':
+			$controller = new C_Edit();
+			break;
+		default:
+			$controller = new C_Page();
+	}
+}
+else
+{
+	$controller = new C_Page();
+}
+$controller->Request($action, $left);
+
+
+?>
